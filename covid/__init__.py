d7728862139148e1e4d40aedb5e0c2cb69dd5d50
Ian Warren <i.warren@auckland.ac.nz>
2020-07-26 15:20:41 +1200

    Styling change.

--- covid/__init__.py
+++ covid/__init__.py
@@ -21,7 +21,7 @@ def create_app(test_config=None):
 
     # Configure the app from configuration-file settings.
     app.config.from_object('config.Config')
-    data_path = 'application\\adapters\\data'
+    data_path = 'covid\\adapters\\data'
 
     if test_config is not None:
         # Load test configuration, and override any configuration settings.
